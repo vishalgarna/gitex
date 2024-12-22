c3f94547173d7f4f8c7732b078d635cf453fa8e0
vishalTheGreat <vishalgarna93@gmail.com>
2024-12-22 14:37:09 +0530

    cng port1

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.get('*', (req, res) => {
 
 
 
-app.listen(5003, () => {
-    console.log('Server is running on port 5003');
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
 
 
